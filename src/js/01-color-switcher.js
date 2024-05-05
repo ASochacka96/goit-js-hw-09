54290b641d692e0bee2da4cb2313f83b2ae8c72a
Anna <a.sochacka96@gmail.com>
2024-05-05 12:26:28 +0200

    Solution of homework
    Solution of homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+// Pobieranie elementów przycisków i body
+const startButton = document.querySelector('[data-start]'); 
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let intervalId; //zmienna interwału czasowego
+
+//Event listenery dodane do przycisków start i stop 
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
+
+
+function startColorChange() { //funkcja rozpoczynająca zmianę kolorów tła
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor(); //funkcja zmieniająca kolor
+    }, 1000); //zmiana koloru co 1sekundę
+    startButton.disabled = true; //wyłączenie przycisku start po jednokrotnym naciśnięciu 
+}
+
+function stopColorChange() { //funkcja zatrzymująca zmianę kolorów
+    clearInterval(intervalId); //zresetowanie odliczania 
+    startButton.disabled = false; //włączenie przysicku start
+}
+
+//funkcja do zmiany kolorów tła
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
